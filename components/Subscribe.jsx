b6b7386cbec921836f4ad861a675a5bb3ebaa327
Piotr Maciejewski <pether.maciejewski@gmail.com>
2023-08-21 16:03:44 +0200

    subscribe changes

--- components/Subscribe.jsx
+++ components/Subscribe.jsx
@@ -7,8 +7,8 @@ const Subscribe = () => {
       id="contact-us"
     >
       <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">
-        Sign Up from
-        <span className="text-coral-red">Updates</span> & Newsletter
+        Sign Up for 
+        <span className="text-coral-red"> Updates</span> & Newsletter
       </h3>
 
       <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
